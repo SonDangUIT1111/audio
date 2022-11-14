6954efef76780582b3ee8dc94ecdd8e0532421bf
Dang Thai Son <sondanguit@gmail.com>
2022-11-14 23:25:26 +0700

    firstcommit

---
+++ Audio/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Audio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+		private MediaPlayer mediaPlayer = new MediaPlayer();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			
+			DispatcherTimer timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += timer_Tick;
+			timer.Start();
+		}
+		private void btnOpenAudioFile_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+			if (openFileDialog.ShowDialog() == true)
+			{
+				string filename = openFileDialog.FileName;
+				listnhac.Items.Add(filename);
+
+			}
+		}
+
+		void timer_Tick(object sender, EventArgs e)
+		{
+			if (mediaPlayer.Source != null)
+				lblStatus.Content = String.Format("{0} / {1}", 
+					mediaPlayer.Position.ToString(@"mm\:ss"),
+					mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+			else
+				lblStatus.Content = "No file selected...";
+		}
+
+		private void btnPlay_Click(object sender, RoutedEventArgs e)
+		{
+			mediaPlayer.Play();
+		}
+
+		private void btnPause_Click(object sender, RoutedEventArgs e)
+		{
+			mediaPlayer.Pause();
+		}
+
+		private void btnStop_Click(object sender, RoutedEventArgs e)
+		{
+			mediaPlayer.Stop();
+		}
+
+        private void listnhac_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+			string uri = e.AddedItems[0].ToString();
+			mediaPlayer.Open(new Uri(uri));
+        }
+    }
+}
